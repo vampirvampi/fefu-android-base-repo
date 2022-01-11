7704b2e742e962aaf6becae85e601e86812ee436
Mikheev Roman <opter12345@gmail.con>
2022-01-11 10:40:57 +1000

    complete transitions

--- app/src/main/java/ru/fefu/activitytracker/LoginActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/LoginActivity.kt
@@ -1,11 +1,20 @@
 package ru.fefu.activitytracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.fefu.activitytracker.databinding.ActivityLoginBinding
+import ru.fefu.activitytracker.databinding.ActivityWelcomeBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbar.setNavigationOnClickListener {
+            startActivity(Intent(this,WelcomeActivity::class.java))
+            this.finish()
+        }
     }
 }
--- app/src/main/java/ru/fefu/activitytracker/RegistrationActivity.kt
+++ app/src/main/java/ru/fefu/activitytracker/RegistrationActivity.kt
@@ -1,11 +1,20 @@
 package ru.fefu.activitytracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.fefu.activitytracker.databinding.ActivityLoginBinding
+import ru.fefu.activitytracker.databinding.ActivityRegistrationBinding
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        binding= ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.toolbar.setNavigationOnClickListener {
+            startActivity(Intent(this,WelcomeActivity::class.java))
+            this.finish()
+        }
     }
 }
